23634eb4064a5dbf944532980ba3a612be310ba3
Hritam Tah <hritam03@gmail.com>
2026-04-28 02:24:25 +0530

    WIP: UPDATED RECOMMENDATION ENGINE

--- src/main/java/com/pg/optimizer/entity/AreaMetadata.java
+++ src/main/java/com/pg/optimizer/entity/AreaMetadata.java
@@ -1,5 +1,10 @@
 package com.pg.optimizer.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class AreaMetadata {
 
     private String areaName;
