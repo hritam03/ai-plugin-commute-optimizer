c9f7f786ccb1a74087533fdf835a27ed212a3b89
Hritam Tah <hritam03@gmail.com>
2026-04-27 23:53:27 +0530

    WIP - Added Ara class for better recommendatin

---
+++ src/main/java/com/pg/optimizer/entity/AreaMetadata.java
@@ -0,0 +1,14 @@
+package com.pg.optimizer.entity;
+
+public class AreaMetadata {
+
+    private String areaName;
+
+    private Double latitude;
+
+    private Double longitude;
+
+    private Integer trafficScore;
+
+    private Integer lifestyleScore;
+}
