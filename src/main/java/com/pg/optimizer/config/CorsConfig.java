084287eb53ecb53b4b0d84d354cac13b7e205a9a
Hritam Tah <hritam03@gmail.com>
2026-05-01 02:58:21 +0530

    Added CorsConfig for Frontend connection

--- src/main/java/com/pg/optimizer/config/CorsConfig.java
+++ src/main/java/com/pg/optimizer/config/CorsConfig.java
@@ -4,13 +4,12 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class AppConfig implements WebMvcConfigurer{
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:5173")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true)
-                .maxAge(3600);
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
     }
 }
