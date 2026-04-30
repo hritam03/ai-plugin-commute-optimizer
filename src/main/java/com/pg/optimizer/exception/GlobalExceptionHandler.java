034d53e40e6919da45895aa775953898f02d1788
Hritam Tah <hritam03@gmail.com>
2026-04-30 22:59:09 +0530

    Standardized API Response Structure

--- src/main/java/com/pg/optimizer/exception/GlobalExceptionHandler.java
+++ src/main/java/com/pg/optimizer/exception/GlobalExceptionHandler.java
@@ -1,7 +1,9 @@
 package com.pg.optimizer.exception;
 
+import com.pg.optimizer.dto.response.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -9,16 +11,49 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(PgNotFoundException.class)
-    public ResponseEntity<String> handlePgNotFound(PgNotFoundException ex) {
+    public ResponseEntity<ApiResponse<Object>> handlePgNotFound(PgNotFoundException ex) {
 
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(ex.getMessage());
+                .body(
+                        ApiResponse.builder()
+                                .success(false)
+                                .message(ex.getMessage())
+                                .data(null)
+                                .build()
+                );
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<String> handleGenericException(Exception ex) {
+    public ResponseEntity<ApiResponse<Object>> handleGenericException(Exception ex) {
 
-        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body("Something went wrong");
+        return ResponseEntity.status(
+                        HttpStatus.INTERNAL_SERVER_ERROR
+                )
+                .body(
+                        ApiResponse.builder()
+                                .success(false)
+                                .message("Something went wrong")
+                                .data(null)
+                                .build()
+                );
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiResponse<Object>> handleValidationException(
+            MethodArgumentNotValidException ex
+    ) {
+        String errorMessage =
+                ex.getBindingResult()
+                        .getFieldError()
+                        .getDefaultMessage();
+
+        return ResponseEntity.badRequest()
+                .body(
+                        ApiResponse.builder()
+                                .success(false)
+                                .message(errorMessage)
+                                .data(null)
+                                .build()
+                );
     }
 }
--- src/main/java/com/pg/optimizer/repository/PgRepository.java
+++ src/main/java/com/pg/optimizer/repository/PgRepository.java
@@ -4,7 +4,6 @@ import com.pg.optimizer.entity.Pg;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 public interface PgRepository extends JpaRepository<Pg, Long> {
 
